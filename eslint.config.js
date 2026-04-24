80ccb68cb2505d5ba9eba6da9462019f290415a5
Darel <johndarel0914@gmail.com>
2026-04-24 17:21:01 +0800

    Initial commit

---
+++ eslint.config.js
@@ -0,0 +1,28 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import { defineConfig } from 'eslint/config'
+
+export default defineConfig([
+    {
+        files: ['**/*.{js,mjs,cjs}'],
+        plugins: { js },
+        extends: ['js/recommended'],
+        languageOptions: { globals: globals.browser },
+        rules: {
+            'no-cond-assign': ['error', 'always'],
+            eqeqeq: ['error', 'always'],
+        },
+    },
+
+    {
+        "overrides": [
+            {
+                "files": ["tests/**/*"],
+                "plugins": ["jest"],
+                "env": {
+                    "jest/globals": true
+                }
+            }
+        ]
+    }
+])
